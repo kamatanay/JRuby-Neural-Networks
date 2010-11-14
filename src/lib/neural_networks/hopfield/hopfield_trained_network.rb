8cd0c4669562e2282525984abe1a3900944ed2c0
Anay Kamat <anay@anay-laptop.(none)>
2010-11-14 15:50:42 +0530

    anay: improved the code of trainer hopfield network.

--- src/lib/neural_networks/hopfield/hopfield_trained_network.rb
+++ src/lib/neural_networks/hopfield/hopfield_trained_network.rb
@@ -6,8 +6,7 @@ class HopfieldTrainedNetwork
 
   def present pattern
     pattern_in_doubles = Matrix.column_vector pattern.to_double
-    row_matrix_array = @weight_matrix.row_vectors.collect{ |vector| Matrix[vector.to_a] }
-    pattern_found = row_matrix_array.collect{ |row_matrix| (row_matrix*pattern_in_doubles).det }
+    pattern_found = (pattern_in_doubles.transpose*@weight_matrix).row(0).to_a
     pattern_found.to_bipolar
   end
 
